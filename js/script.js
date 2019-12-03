a6ab09c6f83428f3d8a6fb85c6b9d899e837f3d8
andrparsons <andrparsons@gmail.com>
2019-12-02 23:41:19 -0500

    javascript for tab control added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Navigation Menu
+
 const closeButton = document.querySelector(".close-nav");
 const openButton = document.querySelector(".open-nav");
 const nav = document.querySelector(".nav");
@@ -9,3 +11,26 @@ closeButton.addEventListener("click", () => {
 openButton.addEventListener("click", () => {
   nav.classList.add("navigation-open");
 });
+
+//Tabs
+
+const tabs = document.querySelector(".tabs");
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+function handleTabClick(event) {
+  tabPanels.forEach(panel => {
+    panel.hidden = true;
+  });
+  tabButtons.forEach(tab => {
+    tab.setAttribute("aria-selected", false);
+  });
+  event.currentTarget.setAttribute("aria-selected", true);
+  const { id } = event.currentTarget;
+  const tabPanel = tabPanels.find(
+    panel => panel.getAttribute("aria-labelledby") === id
+  );
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach(button => button.addEventListener("click", handleTabClick));
